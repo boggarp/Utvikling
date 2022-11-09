e3d910f93703bb83edd291dc06bab18dd4c30844
boggarp <borge.garpestad@vlfk.no>
2022-11-09 10:02:07 +0100

    Update

---
+++ Grunnlegende Koding/leksjon2 - Endre Variabler, For løkke.js
@@ -0,0 +1,19 @@
+// -   Måter å øke en variabel med 1--- //
+// ------------------------------------ //
+let tall = 1
+
+tall = tall + 1
+tall += 1
+tall++
+
+// --  Gjøre noe flere ganger med en for løkke -- //
+// ---------------------------------------------- //
+for (let i=0; i<5; i++) {
+    console.log(i)
+}
+
+//Skrive ut alt i en tabell
+let dyr = ["hest", "geit", "gris", "hjort"]
+for (let i=0; i<dyr.length; i++) {
+    console.log(i,dyr[i])
+}
